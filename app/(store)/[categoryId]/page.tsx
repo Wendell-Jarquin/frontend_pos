e2203490755745f0c86cb9caeadcd48e03df331e
Wendell Jarquin <wendellestudios@gmail.com>
2026-05-15 20:48:56 -0600

    Segundo cargo

--- app/(store)/[categoryId]/page.tsx
+++ app/(store)/[categoryId]/page.tsx
@@ -1,14 +1,17 @@
 type Params = Promise<{ categoryId: string }>;
 
 async function getProducts(categoryId: string) {
-  console.log(categoryId);
+  const url = `${process.env.API_URL}/categories/${categoryId}?products=true`;
+  const req = await fetch(url);
+  const json = await req.json();
+  
 }
 
 
 export default async function CategoryPage({ params }: { params: Params }) {
   const { categoryId } = await params
 
-  await getProducts(categoryId);
+  const products = await getProducts(categoryId);
 
   return <div>StorePage</div>
 }
---
+++ src/schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const ProductSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    image: z.string(),
+    price: z.number(),
+    category: z.number(),
+    categoryId: z.number(),
+});
+
+
+export const CategorySchema = z.object({
+    id: z.number(),
+    name: z.string(),
+});
+
+
+export const CategoryWithProductsResponseSchema = CategorySchema.extend({
+    products: z.array(ProductSchema),
+});
